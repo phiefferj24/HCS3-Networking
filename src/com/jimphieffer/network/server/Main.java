23704b0765e422c5206dbdcfc9ebaa6a8d50d705
TikoTheArmenian <bdoyant23@columbusacademy.org>
2022-04-12 09:33:49 -0400

    Srpite lab

--- src/com/jimphieffer/network/server/Main.java
+++ src/com/jimphieffer/network/server/Main.java
@@ -1,10 +1,29 @@
 package com.jimphieffer.network.server;
 
+import com.jimphieffer.game.Display;
 import com.jimphieffer.game.Game;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JComponent{
     public static void main(String[] args) {
-        Server server = new Server(9000);
+
+
+        Thread t = new Thread(() -> {
+            Server s = new Server(9000);
+            s.listen();
+
+        });
+
+        t.start();
+
+
+        Display d = new Display(800,600);
+        d.run();
+
+
+
 
     }
 }
